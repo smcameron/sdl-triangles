ae556c687173f86ec61e6565ce0919d375c9b213
Stephen M. Cameron <stephenmcameron@gmail.com>
2021-12-23 11:35:27 -0500

    Create SDL window
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- Makefile
+++ Makefile
@@ -1,6 +1,13 @@
 
+PKG_CONFIG?=pkg-config
+
+SDLLIBS:=$(shell $(PKG_CONFIG) sdl2 --libs) -lSDL2_image
+SDLCFLAGS:=$(shell $(PKG_CONFIG) sdl2 --cflags)
+
+MYCFLAGS=-Wall -Wextra --pedantic ${SDLCFLAGS}
+
 sdl-triangles: sdl-triangles.c
-	$(CC) -o sdl-triangles sdl-triangles.c
+	$(CC) ${MYCFLAGS} -o sdl-triangles sdl-triangles.c ${SDLLIBS}
 
 clean:
 	rm -f *.o sdl-triangles
--- sdl-triangles.c
+++ sdl-triangles.c
@@ -18,9 +18,29 @@
 	along with sdl-triangles; if not, write to the Free Software
 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+#include <stdio.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[])
+#include <SDL.h>
+
+#define SCREEN_WIDTH (16 * 64)
+#define SCREEN_HEIGHT (9 * 64)
+
+int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
 {
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		fprintf(stderr, "Unable to initialize SDL (Video):  %s\n", SDL_GetError());
+		return 1;
+	}
+	SDL_Window *window = SDL_CreateWindow("SDL Triangles",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		0, 0, SDL_WINDOW_RESIZABLE);
+	if (!window) {
+		fprintf(stderr, "Could not create window: %s\n", SDL_GetError());
+		return 1;
+	}
+	SDL_SetWindowSize(window, SCREEN_WIDTH, SCREEN_HEIGHT);
+	SDL_ShowWindow(window);
 	return 0;
 }
 
